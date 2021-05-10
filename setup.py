6a06f3c3ee0ee48b4e7ceb6d97e3d7a76163d92b
Clayton Cronk <awesomecronk@gmail.com>
2021-05-10 11:19:19 -0500

    Fixed conflicting dependencies. FYI: DO NOT put builtins such as ctypes in install_requires!!!

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('HISTORY.md') as HISTORYFile:
     HISTORY = HISTORYFile.read()
     
 setup_args = {'name': 'simpleANSI',
-              'version': '1.0.0',
+              'version': '1.0.1',
               'description': 'A simple wrapper for ANSI escape codes',
               'long_description_content_type': 'text/markdown',
               'long_description': README + '\n\n' + HISTORY,
@@ -20,7 +20,7 @@ setup_args = {'name': 'simpleANSI',
               'download_url': 'https://pypi.org/project/simpleANSI'
     }
 
-install_requires = ['ctypes']
+install_requires = []
 
 if __name__ == '__main__':
     setup(**setup_args, install_requires = install_requires)
